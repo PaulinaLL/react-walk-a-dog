ed0d131ad5f4b509d174f22a3d845505181a238c
PaulinaLL <paulinalll90@wp.pl>
2020-05-16 23:54:10 +0200

    Create dogs-list component

---
+++ src/components/dogs-list/dog.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function DogsListItem(props) {
+  function handleClick() {
+    props.onSelect(props.item);
+  }
+
+  return (
+    <li key={props.item.id} className="card" onClick={handleClick}>
+      <div className="card-content">
+        <div className="card-avatar">
+          <img src={props.item.avatar} alt={`${props.item.dogsName}`} />
+        </div>
+
+        <div className="card-personal-data">
+          <p className="card-name">{props.item.dogsName}</p>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default DogsListItem;
